fb6ace65e25f590ec782deba30849f3e551c4bcc
Andrew <andrew64ce@gmail.com>
2016-11-08 14:56:59 -0500

    Pseudo code for Yiyan

--- matlab/rr_example.m
+++ matlab/rr_example.m
@@ -1,24 +1,16 @@
 % Run this AFTER the Robot Raconteur server is started
 robot = RobotRaconteur.Connect('tcp://localhost:10001/phantomXRR/phantomXController');
+base_pose = int16( [400;500;500;500;500] );
+robot.setJointPositions(base_pose);
+pause(1)
 
-% set robot to starting pose
-read_all_joints(robot);
-starting_pose = int16( [500;500;500;500;500]  );
-robot.setJointPositions(starting_pose);
-pause(3) % let robot get into position
-
-% Read all joint positions (should register as ~500 each)
-base_pos = read_all_joints(robot);
-
-% Wobble joint positions
-a = -30;
-b = 30;
+disp 'Going into twitch loop'
+a = -30; b = 30;
 for i=1:1:100
     delta_r = int16(((b-a).*rand(5,1) + [a; a; a; a; a]));
-    new_pose = int16(delta_r + base_pos);
+    new_pose = int16(delta_r + base_pose);
     robot.setJointPositions(new_pose);
-    robot.getJointPosition(int16(1))
-    read_all_joints(robot)
-    pause(2);
+    pause(.1);
+    robot.getJointPositions()
+    pause(1);
 end
-    
--- python_script/RR.py
+++ python_script/RR.py
@@ -4,18 +4,20 @@ import time
 
 # -1 is read start
 # 0xAA55 is read end
-_serial = serial.Serial('/dev/tty.usbserial-A504ECY3', baudrate = 38400)
+_serial = serial.Serial('COM11', baudrate = 38400, timeout=1)
+print "Serial set"
+#raw = _serial.readline()
+#print "after readline"
 
-raw = _serial.readline()
-print "after readline"
-
-data = struct.pack("<hhhhhhhh",-1,2300,500,300,500,500,500,-99)
-#data = struct.pack("<h",-9999)
-print "after struct"
-_serial.write(data)
-print "after write"
+#data = struct.pack("<hhhhhhhh",-1,2300,500,300,500,500,500,-99)
+data = struct.pack("<h",-9999)
+#print "after struct"
+#_serial.write(data)
+#print "after write"
 #time.sleep(2)
 
-for i in range(10):
-	raw = _serial.readline()
-	print raw
+for i in range(20):
+        _serial.write(data)
+        raw = _serial.readline()
+        print "Getting new message"
+        print raw
